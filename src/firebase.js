e162b3cd3ba57190bf4e22d3f79a06f663ff1eed
Manish-404 <19653130+Manish-404@users.noreply.github.com>
2021-03-18 22:07:49 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCpeRIUOF6igCV85QJl7wQ9-5tH10Di1BQ",
+    authDomain: "e-webapp-1beea.firebaseapp.com",
+    projectId: "e-webapp-1beea",
+    storageBucket: "e-webapp-1beea.appspot.com",
+    messagingSenderId: "229714741289",
+    appId: "1:229714741289:web:f0200e3608f48cbdb73438",
+    measurementId: "G-4K5DB4XF36"
+  };
